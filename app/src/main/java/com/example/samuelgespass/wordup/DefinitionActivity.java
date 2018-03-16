0319939005a66fce734d366dde03db222ddfc398
Sam Gespass <30681859+darthtoad@users.noreply.github.com>
2018-03-16 16:34:48 -0700

    add definition activity

--- app/src/main/java/com/example/samuelgespass/wordup/DefinitionActivity.java
+++ app/src/main/java/com/example/samuelgespass/wordup/DefinitionActivity.java
@@ -1,13 +1,54 @@
 package com.example.samuelgespass.wordup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
-public class DefinitionActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class DefinitionActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.wordText)
+    TextView wordText;
+
+    @BindView(R.id.definitionList)
+    ListView listView;
+
+    @BindView(R.id.buttonFavorite)
+    Button button;
+
+    String[] dictionaries = {
+            "ahd-legacy", "wiktionary", "webster", "wordnet", "wordnet"
+    };
+
+    String[] definitions = {
+            "noun, lorem ipsum sin amat de amus triangle cat", "verb, to bear with a bear with a beer", "adjective, a strange fruity colour", "adverb, quickly", "noun, a unit of measurement larger than a cubit but smaller than a mile"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_definition);
+        ButterKnife.bind(this);
+
+        Intent wordIntent = getIntent();
+        String word = wordIntent.getStringExtra("word");
+        wordText.setText(word);
+        DefinitionArrayAdapter definitionArrayAdapter = new DefinitionArrayAdapter(this, android.R.layout.simple_list_item_1, dictionaries, definitions);
+        listView.setAdapter(definitionArrayAdapter);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if (view == button) {
+            Intent wordIntent = getIntent();
+            String word = wordIntent.getStringExtra("word");
+            Intent newIntent = new Intent(DefinitionActivity.this, FavoritesActivity.class);
+            startActivity(newIntent);
+        }
     }
 }
