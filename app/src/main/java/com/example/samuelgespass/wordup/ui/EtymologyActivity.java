6ac243d87461a480b9eecc38fe936d3d4c9bf28e
Sam Gespass <samuel.gespass@gmail.com>
2018-03-21 16:20:32 -0700

    can get to etymology page but only shows an error message

--- app/src/main/java/com/example/samuelgespass/wordup/ui/EtymologyActivity.java
+++ app/src/main/java/com/example/samuelgespass/wordup/ui/EtymologyActivity.java
@@ -1,16 +1,63 @@
 package com.example.samuelgespass.wordup.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 import com.example.samuelgespass.wordup.R;
+import com.example.samuelgespass.wordup.services.WordnikService;
+
+import java.io.IOException;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.Response;
 
 public class EtymologyActivity extends AppCompatActivity {
+    @BindView(R.id.textView)
+    TextView textView;
+
+    @BindView(R.id.titleView)
+    TextView titleView;
+
+    String etymologies;
+    String wordFromIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_etymology);
+        ButterKnife.bind(this);
 
+        Intent wordIntent = getIntent();
+        String wordFromIntent = wordIntent.getStringExtra("word");
+        getEtymologies(wordFromIntent);
+    }
+
+    private void getEtymologies(final String word) {
+        final WordnikService wordnikService = new WordnikService();
+        wordnikService.findEtymology(word, new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
+
+            @Override
+            public void onResponse(Call call, final Response response) throws IOException {
+                etymologies = wordnikService.processEtymologyResults(response);
+                Log.d("Etymologies", etymologies);
+                EtymologyActivity.this.runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        titleView.setText(wordFromIntent);
+                        textView.setText(etymologies);
+                    }
+                });
+            }
+        });
     }
 }
