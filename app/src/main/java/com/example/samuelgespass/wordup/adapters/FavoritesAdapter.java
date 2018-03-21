d9c12d96e0995b3aa3ef1de2ac3ee9611ced43f9
Sam Gespass <samuel.gespass@gmail.com>
2018-03-21 14:26:52 -0700

    change favorites to recycler view

--- app/src/main/java/com/example/samuelgespass/wordup/adapters/FavoritesAdapter.java
+++ app/src/main/java/com/example/samuelgespass/wordup/adapters/FavoritesAdapter.java
@@ -1,35 +1,45 @@
 package com.example.samuelgespass.wordup.adapters;
 
 import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.BaseAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.samuelgespass.wordup.R;
+import com.example.samuelgespass.wordup.ui.DefinitionActivity;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by samuelgespass on 3/16/18.
  */
 
-public class FavoritesAdapter extends BaseAdapter {
+public class FavoritesAdapter extends RecyclerView.Adapter<FavoritesAdapter.FavoritesViewHolder> {
     private Context context;
-    private String[] words;
+    private ArrayList<String> words;
 
-    public FavoritesAdapter (Context context, String[] words) {
+    public FavoritesAdapter (Context context, ArrayList<String> words) {
         this.context = context;
         this.words = words;
     }
 
     @Override
-    public int getCount() {
-        return words.length;
+    public FavoritesAdapter.FavoritesViewHolder onCreateViewHolder(ViewGroup parent, int ViewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.favorites_list_item, parent, false);
+        FavoritesViewHolder viewHolder = new FavoritesViewHolder(view);
+        return viewHolder;
     }
 
     @Override
-    public Object getItem(int i) {
-        return words[i];
+    public void onBindViewHolder(FavoritesAdapter.FavoritesViewHolder holder, int position) {
+        holder.bindWord(words.get(position));
     }
 
     @Override
@@ -38,23 +48,29 @@ public class FavoritesAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
-        LayoutInflater inflater = (LayoutInflater) context
-                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    public int getItemCount() { return words.size(); }
 
-        View gridView;
+    public class FavoritesViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        @BindView(R.id.wordTextView)
+        TextView wordTextView;
+        Context context;
 
-        if (view == null) {
-            gridView = inflater.inflate(R.layout.favorites_grid_item, null);
-            String word = this.words[i];
-
-            TextView wordView = (TextView) gridView
-                    .findViewById(R.id.grid_item_word);
-            wordView.setText(this.getItem(i).toString());
-        } else {
-            gridView = (View) view;
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(this.context, DefinitionActivity.class);
+            intent.putExtra("word", wordTextView.getText());
+            context.startActivity(intent);
         }
 
-        return gridView;
+        public FavoritesViewHolder(View view) {
+            super(view);
+            ButterKnife.bind(this, view);
+            context = view.getContext();
+            view.setOnClickListener(this);
+        }
+
+        public void bindWord(String word) {
+            wordTextView.setText(word);
+        }
     }
 }
--- app/src/main/java/com/example/samuelgespass/wordup/ui/FavoritesActivity.java
+++ app/src/main/java/com/example/samuelgespass/wordup/ui/FavoritesActivity.java
@@ -1,8 +1,11 @@
 package com.example.samuelgespass.wordup.ui;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.widget.GridView;
 
 import com.example.samuelgespass.wordup.R;
@@ -11,13 +14,19 @@ import com.example.samuelgespass.wordup.adapters.FavoritesAdapter;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class FavoritesActivity extends AppCompatActivity {
+    @BindView(R.id.recyclerView)
+    RecyclerView recyclerView;
+    private FavoritesAdapter adapter;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         String[] oldWords = {
-                "Frankfurt", "ABBA", "Platypus", "Chameleon", "Comraderie"
+                "Frankfurt", "thing", "Platypus", "chameleon", "comraderie"
         };
 
         ArrayList<String> wordsList = new ArrayList<>(Arrays.asList(oldWords));
@@ -25,16 +34,18 @@ public class FavoritesActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorites);
+        ButterKnife.bind(this);
         Intent intent = getIntent();
         String word = intent.getStringExtra("word");
         if (!word.equals("")) {
             wordsList.add(word);
         }
-        String[] words = wordsList.toArray(new String[wordsList.size()]);
-        words = wordsList.toArray(words);
 
-        GridView gridView = (GridView) findViewById(R.id.baseGridView);
-        gridView.setAdapter(new FavoritesAdapter(this, words));
+        adapter = new FavoritesAdapter(getApplicationContext(), wordsList);
+        recyclerView.setAdapter(adapter);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(FavoritesActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setHasFixedSize(true);
 
     }
 }
